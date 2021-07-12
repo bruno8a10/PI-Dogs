d74d93f42fa903146efab24a27119d8a71b6cf58
bruno8a10 <bruno8a108a@gmail.com>
2021-07-12 20:31:15 -0300

    se carga perros por front sin la relacion

--- client/src/components/altaDog/AltaDog.js
+++ client/src/components/altaDog/AltaDog.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 //import {getDetalleDog,getTypes, emptyDetalleDog} from "../../actions";
 import {connect} from 'react-redux';
 import logo from "../../img/apiDog.png";
@@ -7,8 +7,40 @@ import { Link } from "react-router-dom";
 import Menu2 from "../menu/Menu2";
 
 function AltaDog(props) {
-  //const id= data.getDetallePokemon(data.match.params.id)
+  //post
+  const [input, setInput] = useState({
+    name: "",
+    height: "",
+    weight: "",
+    speed: "",
+    life_span: "",
+    image: "",
+ })
 
+ function handleChange(e) {
+  setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
+  }
+
+ async function handleSubmit(e) {
+  e.preventDefault()
+  
+  await fetch('http://localhost:3001/dogAlta', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(input),
+  name: "",
+  height: "",
+  weight: "",
+  speed: "",
+  life_span: "",
+  image: "",
+  }) 	
+}
 
 
     return (
@@ -19,18 +51,22 @@ function AltaDog(props) {
      <Menu2/>
       <div>  
         <div className="detalle" >
+        <form className="forms"  onSubmit={handleSubmit}>
            <p>Nombre</p>
-           <input type="text"></input>
+           <input type="text" name="name"  onChange={handleChange}></input>
           <p>Url Imagen</p>
-          <input type="text"></input>
+          <input type="text" name="image"  onChange={handleChange}></input>
           <p>Vida</p>
-          <input type="text"></input>
+          <input type="text"  name="life_span"  onChange={handleChange}></input>
           <p>Peso</p>
-          <input type="text"></input>
+          <input type="text"  name="height"  onChange={handleChange}></input>
           <p>Altura</p>
-          <input type="text"></input>
+          <input type="text"  name="weight"  onChange={handleChange}  ></input>
           <p>Temperamento</p>
-          <button  className="button3">Agregar</button>
+         
+          <button  type="submit" className="button3">Agregar</button>
+         
+          </form> 
         </div>
      
       </div>
