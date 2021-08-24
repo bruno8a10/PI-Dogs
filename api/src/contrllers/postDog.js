044db58a06086a039150b929735ffc24cc374927
bruno8a10 <bruno8a108a@gmail.com>
2021-08-24 01:17:13 -0300

    post andando con relacion

--- api/src/contrllers/postDog.js
+++ api/src/contrllers/postDog.js
@@ -4,27 +4,22 @@ const {Op} = require ("sequelize");
 const axios = require("axios");
 const { Dog,Temperament} = require('../db.js');
 app.post('/', async (req, res) => {
-	const { name, height, weight, life_span, image, temperaments } = req.body;
-	console.log("name :"+name,"h:"+height,"w:"+weight,"l:"+life_span, "imagen :"+image)
+	const { name, height, weight, life_span, image, temp } = req.body;
+	
+	if (temp.length === 0) {
+		return res.sendStatus(500);
+	}
 	try {
-		let [newDog,create] = await Dog.findOrCreate({
-		where:{
+		let newDog = await Dog.create({
 			name,
 			height,
 			weight,
 			life_span,
-			image,
-		}
-		});	
-        //  let arr = temperaments.split(',')
-		// for(let i= 0; i< arr.length;i++){
-		// 	console.log(arr[i])
-		// 	await newDog.addTemperament(arr[i]);
-		// }
-		// no lo toma con el set da vacio await newDog.setTemperament(temperaments);
-		console.log
-		await newDog.addTemperament(temperaments);
+			image
 		
+		});	
+		await newDog.addTemperament(temp);
+
 		return res.json(newDog);
 	} catch (error) {
 		return res.status(404).json(error);
